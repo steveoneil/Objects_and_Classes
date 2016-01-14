e139f0ddb3dfd0ab1d8ef6e67c3ef12d9b152f03
steveoneil <steve@steveoneil.com>
2016-01-14 13:28:41 -0500

    Including Person Class

--- people.rb
+++ people.rb
@@ -1,15 +1,27 @@
-class Student
+class Person
+  attr_accessor :name
+end
+
+class Student < Person
 
   def learn
-    "I get it!"
+    puts "I get it!"
   end
 
 end
 
-class Instructor
+class Instructor < Person
 
   def teach
-    "Everything in Ruby is an Object"
+    puts "Everything in Ruby is an Object"
   end
 
 end
+
+student = Student.new
+student.learn
+student.name = "Ted"
+puts "#{student.name}"
+
+instructor = Instructor.new
+instructor.teach
